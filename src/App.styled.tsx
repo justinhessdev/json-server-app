4d8247fbba38d1c8ff796e4b4126fd15020ed112
Administrator <admin@justins-mbp.lan>
2020-06-08 00:13:05 -0400

    fetch users

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -14,12 +14,22 @@ export const AppWrapper = styled.div`
   height: inherit;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  text-align: center;
+  margin-left: 10em;
 `
 export const AppHeader = styled.div`
   display: flex;
   flex-direction: column;
   height: 20em;
   justify-content: center;
+`
+export const UserWrapper = styled.div`
+  text-align: left;
+`
+export const User = styled.div`
+  cursor: pointer;
+  margin-bottom: 0.5em;
+  font-size: 14px;
+`
+export const UserSpan = styled.span`
+  margin-left: 5em;
 `
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
-import React from 'react'
-import Counter from './Counter'
-import { GlobalStyle, AppWrapper, AppHeader } from './App.styled'
+import React, { useEffect, useState, useCallback } from 'react'
+import {
+  GlobalStyle,
+  AppWrapper,
+  AppHeader,
+  UserWrapper,
+  User,
+  UserSpan
+} from './App.styled'
 
 const App = () => {
+  const [users, setUsers] = useState([])
+  const [userId, setUserId] = useState(-1)
+  const [user, setUser] = useState({})
+  const [sentence, setSentence] = useState('')
+  const [error, setError] = useState('')
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users/')
+      .then((response) => {
+        if (response.status !== 200) {
+          // check for all error messages
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+        return response.json()
+      })
+      .then((data) => setUsers(data))
+      .catch((err) => {
+        setError(err.toString()) // Convert Error object to string
+      })
+  }, [])
+
+  useEffect(() => {}, [])
+
+  const showUserInfo = useCallback((id) => {
+    fetch(`http://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        if (response.status !== 200) {
+          // check for all error messages
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setUserId(data.id)
+        setUser(data)
+      })
+      .catch((err) => {
+        setError(err.toString()) // Convert Error object to string
+      })
+  }, [])
+
   return (
     <AppWrapper>
       <GlobalStyle />
       <AppHeader>
-        <div className='welcome-to-the-jungle'>Hello World</div>
-        <Counter />
+        <h1>Listing users</h1>
+        {!error && users.length === 0 && <div>Loading...</div>}
+        {!error && users.length > 0 && (
+          <UserWrapper>
+            {users.map((user) => (
+              <User key={user.id} onClick={() => showUserInfo(user.id)}>
+                {user.name}
+                {userId === user.id && (
+                  <UserSpan>Emaill: {user.email}</UserSpan>
+                )}
+              </User>
+            ))}
+          </UserWrapper>
+        )}
+        {error && (
+          <div>
+            <div>Error loading data</div>
+            <div>{error}</div>
+          </div>
+        )}
       </AppHeader>
     </AppWrapper>
   )
